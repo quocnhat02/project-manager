c176a1377f736314758593ace78aa600e444d633
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2023-10-11 11:00:35 +0700

    user slice

--- client/src/redux/slices/users/usersSlice.js
+++ client/src/redux/slices/users/usersSlice.js
@@ -33,3 +33,30 @@ export const loginUserAction = createAsyncThunk(
     }
   }
 );
+
+// user slice
+
+const usersSlice = createSlice({
+  name: 'users',
+  initialState,
+  extraReducers: (builder) => {
+    // handle actions
+    // login
+    builder.addCase(loginUserAction.pending, (state, action) => {
+      state.userAuth.loading = true;
+    });
+    builder.addCase(loginUserAction.fulfilled, (state, action) => {
+      state.userAuth.userInfo = action.payload;
+      state.userAuth.loading = false;
+    });
+    builder.addCase(loginUserAction.rejected, (state, action) => {
+      state.userAuth.error = action.payload;
+      state.userAuth.loading = false;
+    });
+  },
+});
+
+// generate reducer
+const usersReducer = usersSlice.reducer;
+
+export default usersReducer;
